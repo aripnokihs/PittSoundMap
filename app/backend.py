2178496fc29d54c8beb0bc230eda743f78a7165b
maxwong3 <42724546+maxwong3@users.noreply.github.com>
2025-09-20 13:29:54 -0400

    api routing (health route)

--- app/backend.py
+++ app/backend.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from app.routes import health
 
 app = FastAPI()
+app.include_router(health.router)
+
 
 API_VERSION = "/v1"
 
 @app.get("/")
 def read_index():
     return FileResponse("app/static/main.html")
-
