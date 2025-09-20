73d84462c5a35680afee8dfa99ebbd9a09d97299
maxwong3 <42724546+maxwong3@users.noreply.github.com>
2025-09-20 13:37:30 -0400

    backend

--- app/backend.py
+++ app/backend.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+import os
+
 from app.routes import health
 
-app = FastAPI()
-app.include_router(health.router)
-
-
 API_VERSION = "/v1"
+app = FastAPI()
+
+app.include_router(health.router, prefix=f"{API_VERSION}/health", tags=["Health"])
+
 
 @app.get("/")
 def read_index():
-    return FileResponse("app/static/main.html")
+    return FileResponse(os.path.join(os.path.dirname(__file__), "static", "main.html"))
